0fd2dd5c0bd17301e741e436e03439f78c2f6215
Jay Hwang <jayhwang0121@gmail.com>
2016-12-14 14:26:51 -0800

    update gitignore

--- frontend/components/about/about_detail.jsx
+++ frontend/components/about/about_detail.jsx
@@ -6,7 +6,7 @@ const AboutDetail = () => {
       <p>
         My journey as an engineer began a little less than a year ago, when I stumbled across an iOS coding tutorial website. It was love at first sight.
         <br/><br/>
-        I went from being able to write simple 'for loops', to building and deploying multiple full-stack web applications in a matter of months. I have an obsession to understand how things work. I thoroughly enjoy having my mind challenged and solving engineering problems that push me past my comfort zone. I love exploring the unknown, especially with fearless, ambitious people that strive to move the world forward.
+        I went from being able to write simple 'for loops', to building and deploying multiple full-stack web applications in a matter of months. I have an obsession to understand how things work under the hood. I thoroughly enjoy having my mind challenged and solving engineering problems that push me past my comfort zone. I love exploring the unknown, especially with fearless, ambitious people that strive to move the world forward.
         <br/><br/>
         You can usually find me drumming away or cruising around the city on two wheels when I need to unwind. Feel free to connect on LinkedIn or leave a message!
       </p>

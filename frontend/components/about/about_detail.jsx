f8247f15f696d9bc2bbe72b332f9ff1b97725efc
Jay Hwang <jayhwang0121@gmail.com>
2016-12-19 18:16:07 -0800

    update about_detail.jsx

--- frontend/components/about/about_detail.jsx
+++ frontend/components/about/about_detail.jsx
@@ -4,7 +4,7 @@ const AboutDetail = () => {
   return (
     <div className='about-detail'>
       <p>
-        My journey as an engineer began a little less than a year ago, when I stumbled across an iOS coding tutorial website. It was love at first sight.
+        It was one of my childhood dreams to know how to code. I was always so fascinated by technology. Being able to create virtually anything you can imagine by typing a set of symbols and characters into a computer. Blows my mind away. My coding journey began when I stumbled across an iOS coding tutorial website. It was love at first sight. I was hooked.
         <br/><br/>
         I went from being able to write simple 'for loops', to building and deploying multiple full-stack web applications in a matter of months. I have an obsession to understand how things work under the hood. I thoroughly enjoy having my mind challenged and solving engineering problems that push me past my comfort zone. I love exploring the unknown, especially with fearless, ambitious people that strive to move the world forward.
         <br/><br/>

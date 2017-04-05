05a766fe8c0f0a966877a91d9ceb7f64c080f497
Jay Hwang <jayhwang0121@gmail.com>
2017-04-05 10:10:34 -0700

    update about

--- frontend/components/about/about_detail.jsx
+++ frontend/components/about/about_detail.jsx
@@ -12,11 +12,7 @@ const AboutDetail = () => {
 
         <br/><br/>
 
-        I realized this was something I wanted to pursue full time, so I started independent studies, even taking full-time programming courses. I had the opportunity to build and work on dozens of projects ranging across the full-stack with some of the brightest people I know, ranging from modern web-apps to games and scripts that automate repetetive tasks.
-
-        <br/><br/>
-
-        Check out some of these projects by clicking on the 'PROJECTS' button in the navbar below!
+        I love helping people bring their ideas to life. Shoot me a message if you have a project you want to build but don't know where to start!
       </div>
     </div>
   );

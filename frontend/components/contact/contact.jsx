db9a9e25f6965ceb5f56d45fe9d1f839181aecf9
Jay Hwang <jayhwang0121@gmail.com>
2017-04-29 10:58:33 -0700

    add contact layout

--- frontend/components/contact/contact.jsx
+++ frontend/components/contact/contact.jsx
@@ -9,11 +9,31 @@ class Contact extends React.Component {
     return (
       <div className='contact-container'>
         <h3 className='title' id='projects-title'>GET IN TOUCH</h3>
-        <p className='contact-text'>
-          I'm looking for new opportunities and would love to chat.
-          If you're interested in working together, please reach out!
-        </p>
 
+        <div className='contact-text'>
+          <p>I'm looking for new opportunities and would love to chat.</p>
+          <p>If you're interested in working together, please reach out!</p>
+        </div>
+
+        <div className='contact-icons'>
+          <div>
+            <img className='contact-icon'
+              src='' />
+            <span className='contact-icon-text'>jayhwang0121@gmail.com</span>
+          </div>
+
+          <div>
+            <img className='contact-icon'
+              src='' />
+            <span className='contact-icon-text'>San Francisco, US</span>
+          </div>
+
+          <div>
+            <img className='contact-icon'
+              src='' />
+            <span className='contact-icon-text'>818 456 7671</span>
+          </div>
+        </div>
       </div>
     );
   }

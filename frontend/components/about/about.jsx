1df19deea3d277b7e22021227662b0d0cc817f24
Jay Hwang <jayhwang0121@gmail.com>
2017-09-29 15:56:38 -0700

    made about component full vh and enlarged content

--- frontend/components/about/about.jsx
+++ frontend/components/about/about.jsx
@@ -16,15 +16,14 @@ const About = () => {
 
         <p>
           I chose to pursue programming because I wanted to tap into technology's potential to contribute to solving humanitarian & social issues.
-          I've had the honor of working on impactful, innovative projects, ranging from a blog dedicated to fighting human trafficking, to launching some of the first server-less, decentralized applications utilizing Blockchains & <a href="https://www.blockstack.org" target="_blank">Blockstack</a>.
         </p>
 
         <p>
-          When I'm not busy telling computers what to do, you can find me writing/playing music with friends, carving mountain roads on a motorcycle or taking apart machines to see how they work
+          When I'm not busy telling computers what to do, you can find me playing music or carving mountains on a motorcycle. I also enjoy taking apart machines & electronics to see how they work.
         </p>
 
         <p>
-          Lately, I've found myself being drawn into the world of Blockchains & Cryptocurrencies.
+          Lately, I've found myself being drawn into the world of Blockchains. I'm very excited about its upcoming advances!
         </p>
 
         <button className='see-skills' onClick={ skillsClick }>
